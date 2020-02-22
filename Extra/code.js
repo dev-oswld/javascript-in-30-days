e9937c3f22de89ebbb05c028ed9e1017ea047642
Oswld <user.oswld@gmail.com>
2020-02-22 15:10:57 -0600

    Self Continuous Learning

--- Extra/code.js
+++ Extra/code.js
@@ -1,1 +1,6 @@
-console.log("Fun experiments"); 
+function learning(){
+    let start = 27; //January 27
+    let end = 21; //February 21
+    let goal = start - end;
+    console.log("Self Continuous Learning =>", goal > 0 ? "Challenge complete 😎" : "Keep doing 😥");
+}
